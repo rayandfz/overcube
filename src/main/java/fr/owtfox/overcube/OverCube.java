a3e17e4e9a829ef3dc7caa88f09d8904da592f3c
Owtfox <laildfz@gmail.com>
2023-08-24 00:24:28 +0200

    Initial commit with report, grant and ungrant command

---
+++ src/main/java/fr/owtfox/overcube/OverCube.java
@@ -0,0 +1,17 @@
+package fr.owtfox.overcube;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OverCube extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
